a04ef757e3fd5826841ccdceb0ebd86be442446d
Sveagruva <sveagruwa@gmail.com>
2021-03-08 17:09:11 +0400

    convertor another take

---
+++ convertor/Conv.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace convertor
+{
+    class Conv : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                place pl = (place)value;
+
+                if (pl.apartment != null)
+                    return "bitche's flat";
+
+                if (pl.land != null)
+                    return "land";
+
+                return "mr mysterio mb house";
+
+            }
+            catch (Exception)
+            {
+                return "";
+                throw;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
